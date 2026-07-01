a235a5114a826d37c10c6df20a61bced9776de60
Noori <noorzafar882@gmail.com>
2026-07-01 20:10:41 +0500

    Utils setup

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,1 +1,9 @@
-class
+class ApiResponse {
+    constructor(statusCode, data, message = "success")
+    this.statusCode = statusCode
+    this.data = data
+    this.message = message
+    this.success = statusCode < 400
+}
+
+export  { ApiResponse}
