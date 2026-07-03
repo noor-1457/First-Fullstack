a7e10a86b9e067b7f421fa2237a951bf48299e52
Noori <noorzafar882@gmail.com>
2026-07-03 17:14:50 +0500

    added jwt methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,4 +64,24 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);          //ye function password ko compare karega
 }
 
+userSchema.methods.generateAccessToken = function(){         //ye function access token generate karega
+return jwt.sign({ 
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    fullname: this.fullname,
+    },process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+)
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+return jwt.sign({ 
+        _id: this._id,
+    },process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+)
+};
+
 export const User = mongoose.model("User", userSchema);
