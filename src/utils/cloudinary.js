0c0b35a5a98e9c310ee3e3776b8e30b70244f0f7
Noori <noorzafar882@gmail.com>
2026-07-03 20:58:16 +0500

    added cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+//yahan pe ham local file ka url leke cloudinary k server pe upload karengay ye code reusable hai jahan marzi use karo or websites pe 
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+    // Configuration
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+
+    //localFilePath is the path of the file which we want to upload on cloudinary server (replace with your local file path)
+    const uplaodOnCloudinary = async (localFilePath) =>{
+        try {
+        if(!localFilePath) return null;
+        //ab ham file uplaod karengay cloudinary k server pe
+        const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        console.log("file uploaded to cloudinary successfully:", uploadResult.url)
+        return uploadResult;
+        }catch(error){
+            fs.unlinkSync(localFilePath); // Delete the local file after upload
+            return null;
+        }
+    }
+
+
+    export { uplaodOnCloudinary };
