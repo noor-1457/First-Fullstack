944ae071190bfa24a2b81555bb2a29a7a71071d1
Noori <noorzafar882@gmail.com>
2026-07-06 23:33:47 +0500

    user controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,52 @@
 import asyncHandler from '../utils/asyncHandler.js';
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 //user route se ye wala controller function call hoga registerUser k liye
 
 const registerUser = asyncHandler(async (req, res) => {     //user registration k liye controller function banaya hai asyncHandler k sath takay error handle ho jaye
-    //get user details from frontend
-    //validation
-    //check if user is registered: username    email
-    // check for images , check for avatar
-    //upload them to cloudinary , avatar
-    // create user object , create entry in db 
-    //remove password and refresh token feild from response 
-    //check for user creation 
-    //return response
-})
+    //1 get user details from frontend
+    //2 validation
+    //3 check if user is registered: username    email
+    //4 check for images , check for avatar
+    //5 upload them to cloudinary , avatar
+    //6 create user object , create entry in db 
+    //7 remove password and refresh token feild from response 
+    //8 check for user creation 
+    //9 return response 
+
+
+    //1
+    const { username, email, fullname, password} = req.body;
+    console.log("email: ", email);
+
+    //2
+    if(
+        [fullname, email, username, password].some((field)=>
+        feild?.trim() === "")
+    ){
+        throw new ApiError(400, "All feilds are required")
+    } 
+
+    //3
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    })
+    if(existedUser) {
+        throw new ApiError(409, "User with email or username already exists.")
+    }
+
+     //4
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    //5
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    })
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from 'express';
 import { registerUser } from '../controllers/user.controller.js';
+import { upload } from "../middlewares/multer.middleware.js;" //ye registerUser se pehle run hoga q k ye middleware hai ipehle ye upload check hoga phir register chalay ga 
 
 const router = Router();
 
-router.route("/register").post(registerUser);   //ab app.js me /api/v1/users/register route kaam karega aur registerUser controller function ko call karega
+router.route("/register").post(
+    upload.fields([             //ye uplaod middleware tab kaam kare ga agar images uplaod hongay
+    {
+        name: "avatar",
+        maxCount: 1
+    },
+    {
+        name: "coverImage",
+        maxCount: 1
+    }
+    ]),
+    registerUser
+);   //ab app.js me /api/v1/users/register route kaam karega aur registerUser controller function ko call karega
 
 export default router;
