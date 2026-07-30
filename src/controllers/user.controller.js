b2a20e26f93659c6f8a34788176dc5ef7f8918f0
Noori <noorzafar882@gmail.com>
2026-07-31 00:05:27 +0500

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -43,10 +43,10 @@ const registerUser = asyncHandler(async (req, res) => {
   const avatarLocalPath = req.files?.avatar[0]?.path; //ider user ki image or avatar dekho local db me hai to uska path variable me store hoga
   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-  let coverImageLocalPath:
+  let coverImageLocalPath;
   if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0 ){
     coverImageLocalPath = req.files.coverImage[0].path
-  }
+  };
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required"); //agar avatar ni hai to error
   }
